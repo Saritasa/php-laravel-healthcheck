a90ed7ec2de181909e4c45efe9d9b4fe8d5be00e
eugenevezhlivtsev <eugene.vezhlivtsev@saritasa.com>
2020-01-21 11:05:39 +0500

    Add S3 checker

--- config/health_check.php
+++ config/health_check.php
@@ -2,10 +2,12 @@
 
 use Saritasa\LaravelHealthCheck\Checkers\DatabaseHealthChecker;
 use Saritasa\LaravelHealthCheck\Checkers\RedisHealthChecker;
+use Saritasa\LaravelHealthCheck\Checkers\S3HealthChecker;
 
 return [
     'checkers' => [
         'database' => DatabaseHealthChecker::class,
         'redis' => RedisHealthChecker::class,
+        's3' => S3HealthChecker::class,
     ],
 ];
